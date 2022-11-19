c1fe787df9392514ec2fbfae149c5ea14a258f14
Dog72 <z2657272578@gmail.com>
2022-11-20 00:12:06 +0800

    补充 资源文件归档实体类

---
+++ src/main/java/top/shareus/common/domain/ArchivedFile.java
@@ -0,0 +1,120 @@
+package top.shareus.common.domain;
+
+import java.util.Date;
+
+
+/**
+ * 存档文件
+ *
+ * @author 17602
+ * @date 2022/11/19
+ */
+public class ArchivedFile {
+    /**
+     * 主键id 为uuid
+     */
+    private String id;
+    
+    /**
+     * 文件名
+     */
+    private String name;
+    
+    /**
+     * 长度
+     */
+    private Long size;
+    
+    /**
+     * MD5
+     */
+    private String md5;
+    
+    /**
+     * 源路径
+     */
+    private String originUrl;
+    
+    /**
+     * 存档路径
+     */
+    private String archiveUrl;
+    
+    /**
+     * 归档日期
+     */
+    private Date archiveDate;
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Long getSize() {
+        return size;
+    }
+    
+    public void setSize(Long size) {
+        this.size = size;
+    }
+    
+    public String getMd5() {
+        return md5;
+    }
+    
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
+    
+    public String getOriginUrl() {
+        return originUrl;
+    }
+    
+    public void setOriginUrl(String originUrl) {
+        this.originUrl = originUrl;
+    }
+    
+    public String getArchiveUrl() {
+        return archiveUrl;
+    }
+    
+    public void setArchiveUrl(String archiveUrl) {
+        this.archiveUrl = archiveUrl;
+    }
+    
+    public Date getArchiveDate() {
+        return archiveDate;
+    }
+    
+    public void setArchiveDate(Date archiveDate) {
+        this.archiveDate = archiveDate;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", size=").append(size);
+        sb.append(", md5=").append(md5);
+        sb.append(", originUrl=").append(originUrl);
+        sb.append(", archiveUrl=").append(archiveUrl);
+        sb.append(", archiveDate=").append(archiveDate);
+        sb.append("]");
+        return sb.toString();
+    }
+}
