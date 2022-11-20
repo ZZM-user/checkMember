43bb0e35d7229509cea0d0048654c476f20c11ee
Dog72 <z2657272578@gmail.com>
2022-11-20 12:56:24 +0800

    简化 Mybatis-Plus可操作范围

--- src/main/java/top/shareus/util/MybatisPlusUtils.java
+++ src/main/java/top/shareus/util/MybatisPlusUtils.java
@@ -2,7 +2,6 @@ package top.shareus.util;
 
 import com.baomidou.mybatisplus.core.MybatisConfiguration;
 import com.baomidou.mybatisplus.core.MybatisSqlSessionFactoryBuilder;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.logging.stdout.StdOutImpl;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.SqlSession;
@@ -10,7 +9,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import top.shareus.common.domain.ArchivedFile;
 import top.shareus.common.mapper.ArchivedFileMapper;
 
 import javax.sql.DataSource;
@@ -23,24 +21,17 @@ public class MybatisPlusUtils {
     
     public static final SqlSessionFactory sqlSessionFactory = initSqlSessionFactory();
     
-    public static void main(String[] args) {
-        try (SqlSession session = sqlSessionFactory.openSession(true)) {
-            ArchivedFileMapper mapper = session.getMapper(ArchivedFileMapper.class);
-            ArchivedFile archivedFile = new ArchivedFile();
-            mapper.insert(archivedFile);
-        }
-    }
-    
     /**
      * init sql会话工厂
      *
      * @return {@code SqlSessionFactory}
      */
-    public static SqlSessionFactory initSqlSessionFactory() {
+    private static SqlSessionFactory initSqlSessionFactory() {
         DataSource dataSource = dataSource();
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
         Environment environment = new Environment("Production", transactionFactory, dataSource);
         MybatisConfiguration configuration = new MybatisConfiguration(environment);
+        // 注册mapper
         configuration.addMapper(ArchivedFileMapper.class);
         configuration.setLogImpl(StdOutImpl.class);
         return new MybatisSqlSessionFactoryBuilder().build(configuration);
@@ -51,7 +42,7 @@ public class MybatisPlusUtils {
      *
      * @return {@code DataSource}
      */
-    public static DataSource dataSource() {
+    private static DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
         dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
         dataSource.setUrl("jdbc:mysql://124.220.67.51:3306/mirai");
@@ -67,7 +58,7 @@ public class MybatisPlusUtils {
      *
      * @return {@code T}
      */
-    public static <T extends BaseMapper> T getMapper(Class<T> tClass) {
+    public static <T> T getMapper(Class<T> tClass) {
         try (SqlSession session = MybatisPlusUtils.sqlSessionFactory.openSession(true)) {
             return session.getMapper(tClass);
         } catch (Exception e) {
