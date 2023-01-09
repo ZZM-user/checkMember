0a7ef1b7911a40a99f8d85d539ec0f9ba51b63a7
17602 <2657272578@qq.com>
2023-01-09 19:06:33 +0800

    优化 排版

--- src/main/java/top/shareus/job/archived/Day.java
+++ src/main/java/top/shareus/job/archived/Day.java
@@ -33,8 +33,8 @@ public class Day implements Task {
 
         MessageChainBuilder builder = new MessageChainBuilder();
         builder.add("昨日资源群归档文件数量：" + hasArchived);
-        builder.add("昨日资源群求文数量：" + queryLogs.size());
-        builder.add("昨日资源群未完成的求文：" + unfinishedQuery.size());
+        builder.add("\n昨日资源群求文数量：" + queryLogs.size());
+        builder.add("\n昨日资源群未完成的求文：" + unfinishedQuery.size());
 
         Bot bot = BotManager.getBot();
         Group group = bot.getGroupOrFail(GroupsConstant.TEST_GROUPS.get(0));
